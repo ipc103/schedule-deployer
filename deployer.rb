dea5f72dc322a960d779e6d21b9b243f37c37c69
ipc103 <iancandysemail@gmail.com>
2016-01-12 17:25:05 -0500

    deploys with hard coded repo and credentials

--- deployer.rb
+++ deployer.rb
@@ -1,3 +1,18 @@
 require 'bundler'
 Bundler.require
 Dotenv.load
+
+class Deployer
+  attr_reader :client, :repo
+
+  def initialize
+    @client = Octokit::Client.new(:access_token => ENV['access_token'])
+    @repo = 'ipc103/test-schedule'
+  end
+
+  def post_todays_schedule
+    today = Date.today.strftime('%B %d, %Y')
+    client.merge(repo, 'master', 'tomorrow', {commit_message: "Deployed Schedule for #{today}"})
+  end
+
+end
